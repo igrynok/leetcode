f2c9d2ef05fdcb47b6d710bedf61d1935cfd8cd6
igrynok <ibragimova.aliya@gmail.com>
2023-08-13 17:38:10 +0200

    BFS: painting gardens, refactoring, simple solution.

--- bfs/flower_planting.py
+++ bfs/flower_planting.py
@@ -3,37 +3,6 @@ from collections import defaultdict, deque
 
 class Solution(object):
 
-    def get_uncolored(self, answer):
-        for index, a in enumerate(answer):
-            if a == -1:
-                return index + 1
-        return -1
-
-    def bfs(self, start, graph, answer):
-
-        queue = deque([(start, 1)])
-        answer[start - 1] = 1
-
-        while len(queue) > 0:
-            node = queue.popleft()
-
-            for child in graph[node[0]]:
-                if answer[child - 1] != -1:
-                    continue
-
-                colors = [color for color in [1, 2, 3, 4] if color != node[1]]
-                for neighbour in graph[child]:
-
-                    if answer[neighbour - 1] != -1:
-                        if answer[neighbour - 1] in colors:
-                            colors.remove(answer[neighbour - 1])
-
-                color = colors.pop()
-                answer[child - 1] = color
-                queue.append((child, color))
-
-        return answer
-
     def gardenNoAdj(self, n, paths):
         """
         :type n: int
@@ -48,29 +17,12 @@ class Solution(object):
 
         answer = [-1]*n
 
-        while self.get_uncolored(answer) != -1 :
-            uncolored = self.get_uncolored(answer)
-            self.bfs(uncolored, graph, answer)
-
-        return answer
-
-    def gardenNoAdj(self, n, paths):
-        """
-        :type n: int
-        :type paths: List[List[int]]
-        :rtype: List[int]
-        """
-
-        graph = defaultdict(list)
-        for path in paths:
-            graph[path[0]].append(path[1])
-            graph[path[1]].append(path[0])
-
-        answer = [-1]*n
-
-        while self.get_uncolored(answer) != -1 :
-            uncolored = self.get_uncolored(answer)
-            self.bfs(uncolored, graph, answer)
+        for i in range(1, n + 1):
+            colors = [1, 2, 3, 4]
+            for neighbour in graph[i]:
+                if answer[neighbour - 1] != -1 and answer[neighbour - 1] in colors:
+                    colors.remove(answer[neighbour - 1])
+            answer[i - 1] = colors.pop()
 
         return answer
 
