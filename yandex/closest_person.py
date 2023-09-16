dc27b7dc1873f069059e1227658957ff426f6896
igrynok <ibragimova.aliya@gmail.com>
2023-09-16 12:55:54 +0200

    Closest person: maximise the closest person.

---
+++ yandex/closest_person.py
@@ -0,0 +1,27 @@
+# [1, 0]
+# 1
+
+# [0, 0, 1]
+# 2
+
+# [1, 0, 0, 0, 1, 0, 1]
+# 2
+
+class Solution:
+
+    def maxDistToClosest(self, seats: List[int]) -> int:
+
+        prev = -1
+        max_dist = 0
+
+        for i, seat in enumerate(seats):
+            if seat == 1:
+                if prev != -1:
+                    max_dist = max(max_dist, int((i - prev)/2))
+                else:
+                    max_dist = i
+                prev = i
+
+        max_dist = max(max_dist, len(seats) - prev - 1)
+
+        return max_dist
