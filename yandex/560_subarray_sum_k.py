b3e6e4f1d01c7e70bade32cebdd268059b321f8e
igrynok <ibragimova.aliya@gmail.com>
2023-09-26 18:26:42 +0200

    Yandex: count subarrays with the sum equal k, optimised solution.

--- yandex/560_subarray_sum_k.py
+++ yandex/560_subarray_sum_k.py
@@ -10,16 +10,21 @@
 # [1, 2, 3], k = 3
 # 2
 
+from collections import defaultdict
+
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
         count = 0
+        prefix_sum = 0
+        dd = defaultdict(int)
+        dd[0] = 1
 
         for i in range(len(nums)):
-            prefix_sum = 0
-            for j in range(i, len(nums)):
-                prefix_sum += nums[j]
-                if prefix_sum == k:
-                    count += 1
+            prefix_sum += nums[i]
+            if (prefix_sum - k) in dd:
+                count += dd[prefix_sum - k]
+            dd[prefix_sum] += 1
 
         return count
