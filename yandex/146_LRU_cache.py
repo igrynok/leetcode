7fe6aeedb11eeacd6100e14b3bb3ab3cac07add7
igrynok <ibragimova.aliya@gmail.com>
2023-10-02 11:49:06 +0200

    Yandex: LRU cache, small fix.

--- yandex/146_LRU_cache.py
+++ yandex/146_LRU_cache.py
@@ -63,7 +63,7 @@ class LRUCache:
 
             self.volume += 1
 
-        # Your LRUCache object will be instantiated and called as such:
+# Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
