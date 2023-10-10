7e3111469be284c65b515c91a848409cfb32fdfb
igrynok <ibragimova.aliya@gmail.com>
2023-10-10 15:33:13 +0200

    Yandex: Implement Rand10() Using Rand7(). Works not for all cases.

---
+++ yandex/470_rand10.py
@@ -0,0 +1,14 @@
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to
+
+def rand7():
+    pass
+
+
+class Solution:
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        return int(rand7() / 7 * 10)
