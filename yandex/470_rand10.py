bfd970ef9d10dc0dceb0ec15f47b25d80ea495c7
igrynok <ibragimova.aliya@gmail.com>
2023-10-11 09:56:29 +0200

    Yandex: Implement Rand10() Using Rand7(). Another Solution.

--- yandex/470_rand10.py
+++ yandex/470_rand10.py
@@ -5,20 +5,16 @@
 def rand7():
     pass
 
-
 class Solution:
     def rand10(self):
         """
         :rtype: int
         """
+        space = [[1,2, 3, 4, 5, 6, 7], [8, 9, 10]]
+
         while True:
+            num1 = rand7()
+            num2 = rand7()
 
-            col = rand7()
-            row = rand7()
-
-            num = (row - 1) * 7 + col
-
-            if num <= 40:
-                break
-
-        return num % 10 + 1
+            if num2 == 1 or (num2 == 2 and num1 <= 3):
+                return space[num2-1][num1-1]
