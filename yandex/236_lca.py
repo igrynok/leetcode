2965cb55d82f3f16232d5ddfd8be3227ff3d655a
igrynok <ibragimova.aliya@gmail.com>
2023-10-30 10:53:36 +0100

    Yandex: lowest common ancestor, beautiful O(n) solution.

--- yandex/236_lca.py
+++ yandex/236_lca.py
@@ -1,37 +1,30 @@
 # Definition for a binary tree node.
-
-class TreeNode:
+ class TreeNode:
      def __init__(self, x):
          self.val = x
          self.left = None
          self.right = None
 
-
 class Solution:
-
-    def search(self, root, node):
-        if not root:
-            return False
-        if root.val == node.val:
-            return True
-        return self.search(root.right, node) or self.search(root.left, node)
 
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        result = root
-
         if not root:
-            return None
+            return
 
         if root.val == p.val or root.val == q.val:
-            return result
+            return root
 
-    is_left_node1 = self.search(root.left, p)
-        is_left_node2 = self.search(root.left, q)
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-        if is_left_node1 and is_left_node2:
-            result = self.lowestCommonAncestor(root.left, p, q)
-        elif not is_left_node1 and not is_left_node2:
-            result = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
 
-        return result
+        if left:
+            return left
+
+        if right:
+            return right
+
+        return None
