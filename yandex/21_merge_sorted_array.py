ae302a6ddb794e5a527a4eaf5c486b3db02d5e03
igrynok <ibragimova.aliya@gmail.com>
2023-11-06 15:04:27 +0100

    Yandex: Add imports.

--- yandex/21_merge_sorted_array.py
+++ yandex/21_merge_sorted_array.py
@@ -6,6 +6,8 @@
 
 # [1, 2, 3, 0, 0, 0] [2, 5, 6]
 # [1, 2, 2, 3, 5, 6]
+from typing import List
+
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
