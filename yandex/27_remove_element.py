ad6ed8fd133961b8cef863f2f77705976e7e4388
igrynok <ibragimova.aliya@gmail.com>
2023-12-15 15:54:31 +0100

    Yandex: Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.

---
+++ yandex/27_remove_element.py
@@ -0,0 +1,27 @@
+# [] 1
+# 0
+
+# [2] 1
+# 1
+
+# [2] 2
+# 0
+
+# [3, 2, 2, 3] 3
+# 2
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        i = 0
+        n = len(nums)
+
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -= 1
+            else:
+                i += 1
+
+        return n
