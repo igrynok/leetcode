6c67a98176f26e37144968ac8b5829345753f6eb
igrynok <ibragimova.aliya@gmail.com>
2023-12-16 20:44:32 +0100

    Yandex: Given the root of a binary tree, return its maximum depth. Add missing imports and class definition.

--- yandex/104_max_tree_depth.py
+++ yandex/104_max_tree_depth.py
@@ -1,12 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
         def max_depth(node, level):
             if not node:
                 return level
