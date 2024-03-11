d2015b921e79feafe08b4f47066ac6c58be67760
igrynok <ibragimova.aliya@gmail.com>
2024-03-11 14:03:57 +0100

    Non optimal solution for jump game problem. You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
    Return true if you can reach the last index, or false otherwise.

---
+++ google/55_jump_game.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        can_jump = False
+
+        @cache
+        def dfs(index):
+
+            nonlocal can_jump
+
+            if index == len(nums) - 1:
+                can_jump = True
+
+            max_jump = nums[index]
+            for i in range(1, max_jump + 1):
+                if index + i < len(nums):
+                    dfs(index + i)
+
+        dfs(0)
+        return can_jump
