7e9e607ea36f38627881e554c508b54607e01ca1
igrynok <ibragimova.aliya@gmail.com>
2024-03-12 16:31:52 +0100

    Accepted solution.

--- google/55_jump_game.py
+++ google/55_jump_game.py
@@ -5,20 +5,18 @@ from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
-        can_jump = False
-
         @cache
         def dfs(index):
 
-            nonlocal can_jump
-
             if index == len(nums) - 1:
-                can_jump = True
+                return True
 
             max_jump = nums[index]
             for i in range(1, max_jump + 1):
                 if index + i < len(nums):
-                    dfs(index + i)
+                    if dfs(index + i):
+                        return True
 
-        dfs(0)
-        return can_jump
+            return False
+
+        return dfs(0)
