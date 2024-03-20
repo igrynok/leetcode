9ea4f24082f77149b6278cffae5b2d439e4a2b81
igrynok <ibragimova.aliya@gmail.com>
2024-03-20 12:40:30 +0100

    Accepted Solution. No while loops. Very elegant but not the most time efficient.

--- google/76_min_window_substr.py
+++ google/76_min_window_substr.py
@@ -13,23 +13,26 @@ class Solution:
 
         counter = defaultdict(int)
         counter.update(Counter(t))
-        slow, fast = 0, 0
+
+        slow = 0
         min_subs = ""
 
-        while fast < len(s):
+        for fast in range(len(s)):
 
             if s[fast] in counter.keys():
                 counter[s[fast]] -= 1
 
             if self.is_empty(counter):
-                if not min_subs or len(min_subs) > fast - slow + 1:
-                    min_subs = s[slow:fast+1]
-                counter[s[slow]] += 1
-                slow += 1
-                while slow < fast and not s[slow] in counter.keys():
-                    slow += 1
-                counter[slow] -= 1
+                for slow_i in range(slow, fast + 1):
+                    if s[slow_i] in counter.keys() and counter[s[slow_i]] < 0:
+                        counter[s[slow_i]] += 1
+                    elif not s[slow_i] in counter.keys():
+                        continue
+                    else:
+                        slow = slow_i
+                        break
 
-            fast += 1
+                if not min_subs or len(min_subs) > fast - slow + 1:
+                    min_subs = s[slow:fast + 1]
 
         return min_subs
