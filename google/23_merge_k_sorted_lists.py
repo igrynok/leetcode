78eddf82028ba15a989a0980a45b15649d3dc2b2
igrynok <ibragimova.aliya@gmail.com>
2024-03-28 09:36:32 +0100

    Solution with heapq. You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
    Merge all the linked-lists into one sorted linked-list and return it.

---
+++ google/23_merge_k_sorted_lists.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+import heapq
+from typing import List, Optional
+
+
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        head = point = ListNode()
+        heap = []
+        for node in lists:
+            if node:
+                heapq.heappush(heap, (node.val, id(node), node))
+
+        while heap:
+            _, _, min_node = heapq.heappop(heap)
+            point.next = min_node
+            point = point.next
+            if min_node.next:
+                heapq.heappush(heap, (min_node.next.val, id(min_node.next), min_node.next))
+
+        return head.next
