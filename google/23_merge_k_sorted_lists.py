58fb7c248abf2868e5f061cff35421c4aa8b46b6
igrynok <ibragimova.aliya@gmail.com>
2024-04-02 16:44:44 +0200

    Small refactoring.

--- google/23_merge_k_sorted_lists.py
+++ google/23_merge_k_sorted_lists.py
@@ -4,9 +4,9 @@ from typing import List, Optional
 
 
 class ListNode:
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
