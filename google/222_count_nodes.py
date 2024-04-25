ba7f9ce91f95779d39fff795e24ef7a5bbfd2fa1
igrynok <ibragimova.aliya@gmail.com>
2024-04-25 12:33:58 +0200

    Given the root of a complete binary tree, return the number of the nodes in the tree.

---
+++ google/222_count_nodes.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node):
+
+            if not node:
+                return 0
+
+            return 1 + dfs(node.left) + dfs(node.right)
+
+        return dfs(root)
