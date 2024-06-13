1fac7f1232c2d23f517050cf631b821bfe86ad8e
igrynok <ibragimova.aliya@gmail.com>
2024-06-13 19:43:58 +0200

    Refactoring

--- google/951_flip_equiv_tree.py
+++ google/951_flip_equiv_tree.py
@@ -3,10 +3,11 @@ from typing import Optional
 
 
 class TreeNode:
- def __init__(self, val=0, left=None, right=None):
-     self.val = val
-     self.left = left
-     self.right = right
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
