bf29ef604b0426e01f5ef2404fbafcd1261d5ded
igrynok <ibragimova.aliya@gmail.com>
2024-06-23 01:52:28 +0200

    Time Limit Exceeded Solution.

--- google/212_word_search2.py
+++ google/212_word_search2.py
@@ -8,30 +8,28 @@ class Solution:
         prefix_dict = defaultdict(list)
         for word in words:
             for i in range(len(word)):
-                prefix_dict[word[:i + 1]].append(word)
+                prefix_dict[word[:i+1]].append(word)
+
 
         answer = []
-
-        def dfs(prefix, cell, visited):
+        def dfs(prefix, cell):
 
             if not prefix in prefix_dict:
                 return
             elif prefix in prefix_dict[prefix]:
                 answer.append(prefix)
-                return
 
+            letter = board[cell[1]][cell[0]]
             dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+            board[cell[1]][cell[0]] = '#'
             for d in dirs:
-                if 0 <= cell[0] + d[0] < len(board[0]) and 0 <= cell[1] + d[1] < len(board) and not (cell[0] + d[0],
-                                                                                                     cell[1] + d[
-                                                                                                         1]) in visited:
+                if 0 <= cell[0] + d[0] < len(board[0]) and 0 <= cell[1] + d[1] < len(board):
                     new_cell = (cell[0] + d[0], cell[1] + d[1])
-                    visited.add(new_cell)
-                    dfs(prefix + board[new_cell[1]][new_cell[0]], new_cell, visited)
+                    dfs(prefix + board[new_cell[1]][new_cell[0]], new_cell)
+            board[cell[1]][cell[0]] = letter
 
         for i in range(len(board)):
             for j in range(len(board[0])):
-                cell = (j, i)
-                dfs(board[i][j], cell, set())
+                dfs(board[i][j], (j, i))
 
-        return answer
+        return list(set(answer))                    
