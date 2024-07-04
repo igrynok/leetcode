f50357a38e182836059d1b42d3449344ceff4e96
igrynok <ibragimova.aliya@gmail.com>
2024-07-04 15:06:10 +0200

    Dynamic programming solution.

--- google/5_longest_palindrome_substring.py
+++ google/5_longest_palindrome_substring.py
@@ -1,34 +1,14 @@
-# "a"
-# "a"
-
-# "babad"
-# "bab"
-
-# "cbbd"
-# "bb"
-
 class Solution:
-
-    def is_palindrome(self, word, start, end):
-
-        while start <= end:
-            if word[start] != word[end]:
-                return False
-            start += 1
-            end -= 1
-
-        return True
-
     def longestPalindrome(self, s: str) -> str:
 
-        longest = ""
+        dp = [[False]*len(s) for i in range(len(s))]
+        start, end = 0, 0
 
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                if self.is_palindrome(s, i, j):
-                    if j - i + 1 > len(longest):
-                        longest = s[i:j + 1]
-                    if len(longest) == len(s):
-                        return longest
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i + 1, len(s)):
+                dp[i][j] = s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1])
+                if dp[i][j] and j - i > end - start:
+                    start = i
+                    end = j
 
-        return longest
+        return s[start:end + 1]      
