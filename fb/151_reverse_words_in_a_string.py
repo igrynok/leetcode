6033c2c36a853d6dc4a5b9ab969734adbba21668
igrynok <ibragimova.aliya@gmail.com>
2024-07-13 08:35:49 +0200

    Approach with built-in functions.

--- fb/151_reverse_words_in_a_string.py
+++ fb/151_reverse_words_in_a_string.py
@@ -1,8 +1,3 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        arr = s.split()
-        reversed_word = []
-        for i in range(len(arr) - 1, -1, -1):
-            reversed_word.append(arr[i])
-
-        return " ".join(reversed_word)
+        return " ".join(reversed(s.split()))
