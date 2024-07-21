91c01ada4846fa92b9e8bbfe581a5fca3e59ef75
igrynok <ibragimova.aliya@gmail.com>
2024-07-21 14:00:28 +0200

    Right for the most of the cases.

--- fb/73_set_matrix_zeros.py
+++ fb/73_set_matrix_zeros.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
 
@@ -12,7 +9,7 @@ class Solution:
         rows, cols = len(matrix), len(matrix[0])
 
         def set_zeros(row, col):
-            if row:
+            if row != -1:
                 for k in range(cols):
                     matrix[row][k] = 0
             else:
@@ -25,18 +22,20 @@ class Solution:
         for i in range(rows):
             if i in rows_set:
                 continue
+            row_zeros = False
             for j in range(cols):
                 if j in cols_set:
                     continue
                 if matrix[i][j] == 0:
-                    set_zeros(row=i, col=None)
-                    set_zeros(row=None, col=j)
-                    rows_set.add(i)
+                    set_zeros(row=-1, col=j)
                     cols_set.add(j)
-                    break
+                    row_zeros = True
+            if row_zeros:
+                set_zeros(row=i, col=-1)
+                rows_set.add(i)
 
 
 if __name__ == "__main__":
-    matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+    matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
     Solution().setZeroes(matrix)
     print(matrix)
