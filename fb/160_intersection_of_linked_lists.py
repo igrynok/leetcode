fd827b6c20563d2689dc12cd3a207c292ecf2bfe
igrynok <ibragimova.aliya@gmail.com>
2024-08-01 09:37:55 +0200

    Editorial Solution with no additional memory.

--- fb/160_intersection_of_linked_lists.py
+++ fb/160_intersection_of_linked_lists.py
@@ -7,20 +7,18 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        pA = headA
+        pB = headB
 
-        nodes = set()
+        while pA != pB:
+            pA = headB if pA is None else pA.next
+            pB = headA if pB is None else pB.next
 
-        node = headA
-        while node:
-            nodes.add(node)
-            node = node.next
-
-        intersection = headB
-        while intersection:
-            if intersection in nodes:
-                return intersection
-            intersection = intersection.next
-
-        return None
+        return pA
+        # Note: In the case lists do not intersect, the pointers for A and B
+        # will still line up in the 2nd iteration, just that here won't be
+        # a common node down the list and both will reach their respective ends
+        # at the same time. So pA will be NULL in that case.
