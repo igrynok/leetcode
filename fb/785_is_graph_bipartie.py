e325cdf99b4f6e8e618c5aae9fc6e22cedcfe924
igrynok <ibragimova.aliya@gmail.com>
2024-09-02 15:13:18 +0200

    There is an undirected graph with n nodes, where each node is numbered between 0 and n - 1. You are given a 2D array graph, where graph[u] is an array of nodes that node u is adjacent to. More formally, for each v in graph[u], there is an undirected edge between node u and node v. The graph has the following properties:
    There are no self-edges (graph[u] does not contain u).
    There are no parallel edges (graph[u] does not contain duplicate values).
    If v is in graph[u], then u is in graph[v] (the graph is undirected).
    The graph may not be connected, meaning there may be two nodes u and v such that there is no path between them.
    A graph is bipartite if the nodes can be partitioned into two independent sets A and B such that every edge in the graph connects a node in set A and a node in set B.
    
    Return true if and only if it is bipartite.

---
+++ fb/785_is_graph_bipartie.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def isBipartite(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: bool
+        """
+        set_A, set_B = set(), set()
+
+        for i in range(len(graph)):
+
+            if i in set_A or i in set_B:
+                continue
+
+            queue = deque()
+            visited = set()
+            if len(graph[i]):
+                set_A.add(i)
+                queue.append(i)
+                visited.add(i)
+
+            while queue:
+                node = queue.popleft()
+                for child in graph[node]:
+                    if node in set_A:
+                        if child in set_A:
+                            return False
+                        set_B.add(child)
+                    else:
+                        if child in set_B:
+                            return False
+                        set_A.add(child)
+                    if child in visited:
+                        continue
+                    visited.add(child)
+                    queue.append(child)
+
+        return True
