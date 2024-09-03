7352c0dd3ffad06a7a3bff4041bdf077f7aec9f6
igrynok <ibragimova.aliya@gmail.com>
2024-09-03 14:09:45 +0200

    Given the root of a binary tree, return the vertical order traversal of its nodes' values. (i.e., from top to bottom, column by column).
    If two nodes are in the same row and column, the order should be from left to right.

---
+++ fb/314_BT_vertical_traversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+from collections import defaultdict, deque
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+
+        if not root:
+            return []
+
+        queue = deque([(root, 0)])
+        columns = defaultdict(list)
+        columns[0].append(root.val)
+
+        while queue:
+            node, index = queue.popleft()
+            if node.left:
+                columns[index - 1].append(node.left.val)
+                queue.append((node.left, index - 1))
+            if node.right:
+                columns[index + 1].append(node.right.val)
+                queue.append((node.right, index + 1))
+
+        return [columns[key] for key in sorted(columns.keys())]
