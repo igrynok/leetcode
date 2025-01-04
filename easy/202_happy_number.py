f2b9bda8f10c831fcfff7648788161560448e021
igrynok <ibragimova.aliya@gmail.com>
2025-01-04 16:43:17 +0100

    Write an algorithm to determine if a number n is happy.
    A happy number is a number defined by the following process:
    
    Starting with any positive integer, replace the number by the sum of the squares of its digits.
    Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
    Those numbers for which this process ends in 1 are happy.
    Return true if n is a happy number, and false if not.

---
+++ easy/202_happy_number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        num_set = set()
+        while n not in num_set:
+            num_set.add(n)
+            n = sum([int(digit)**2 for digit in str(n)])
+            if n == 1:
+                return True
+
+        return False
