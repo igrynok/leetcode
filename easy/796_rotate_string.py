e001f05ad72aed59bf191307111ed464b1a30aed
igrynok <ibragimova.aliya@gmail.com>
2025-01-08 21:39:54 +0100

    Very elegant solution!

--- easy/796_rotate_string.py
+++ easy/796_rotate_string.py
@@ -1,26 +1,3 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-
-        if len(s) != len(goal):
-            return False
-
-        for i in range(len(s)):
-            for j in range(len(goal)):
-                if i + j < len(s):
-                    if goal[j] == s[i + j]:
-                        if j == len(goal) - 1:
-                            return True
-                        else:
-                            continue
-                    else:
-                        break
-                else:
-                    if goal[j] == s[i + j - len(s)]:
-                        if j == len(goal) - 1:
-                            return True
-                        else:
-                            continue
-                    else:
-                        break
-
-        return False
+        return len(s) == len(goal) and goal in s + s
