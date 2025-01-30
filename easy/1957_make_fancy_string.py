1af3d43bbc1d27a16e0b582e4529663def42a401
igrynok <ibragimova.aliya@gmail.com>
2025-01-30 11:08:17 +0100

    Yet another Time Limit Exceeded Solution.

--- easy/1957_make_fancy_string.py
+++ easy/1957_make_fancy_string.py
@@ -2,15 +2,17 @@ class Solution:
     def makeFancyString(self, s: str) -> str:
 
         to_remove = []
-        for index, elem in enumerate(s):
-            if index >= len(s) - 2:
-                break
+
+        index = 0
+        while index < len(s) - 2:
             count = 1
-            while count < 3:
-                if elem != s[index + count]:
-                    break
+            while index + count < len(s) and s[index] == s[index + count]:
                 count += 1
-                if count == 3:
-                    to_remove.append(index)
+            fix = index
+            while count >= 3:
+                to_remove.append(fix + count - 1)
+                count -= 1
+                index += 1
+            index += 1
 
         return "".join([elem for index, elem in enumerate(s) if index not in to_remove])
