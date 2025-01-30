172dd49cb138082f9333dd9727b17352f23c2b38
igrynok <ibragimova.aliya@gmail.com>
2025-01-30 11:15:02 +0100

    Accepted solution, no more additional pass through the array.

--- easy/1957_make_fancy_string.py
+++ easy/1957_make_fancy_string.py
@@ -1,18 +1,17 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
 
-        to_remove = []
-
+        new_str = []
         index = 0
-        while index < len(s) - 2:
+        while index < len(s):
+            new_str.append(s[index])
             count = 1
             while index + count < len(s) and s[index] == s[index + count]:
                 count += 1
             fix = index
             while count >= 3:
-                to_remove.append(fix + count - 1)
                 count -= 1
                 index += 1
             index += 1
 
-        return "".join([elem for index, elem in enumerate(s) if index not in to_remove])
+        return "".join(new_str)
