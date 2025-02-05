659b772e2a9f4252f237b35ba3b5a7e695141183
igrynok <ibragimova.aliya@gmail.com>
2025-02-05 16:46:05 +0100

    Refactor.

--- easy/884_uncommon_words.py
+++ easy/884_uncommon_words.py
@@ -1,21 +1,12 @@
-from typing import List
 from collections import Counter
+from typing import List
 
 
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
 
         words1 = s1.split()
-        word_count1 = Counter(words1)
         words2 = s2.split()
-        word_count2 = Counter(words2)
+        word_count = Counter(words1 + words2)
 
-        uncommon = []
-        for w1 in words1:
-            if word_count1[w1] == 1 and w1 not in word_count2:
-                uncommon.append(w1)
-        for w2 in words2:
-            if word_count2[w2] == 1 and w2 not in word_count1:
-                uncommon.append(w2)
-
-        return uncommon
+        return [key for key in word_count.keys() if word_count[key] == 1]
