919a7ffc9d435b72234bce47f95feff95df27b3b
igrynok <ibragimova.aliya@gmail.com>
2025-02-06 22:12:49 +0100

    There are n people standing in a line labeled from 1 to n. The first person in the line is holding a pillow initially. Every second, the person holding the pillow passes it to the next person standing in the line. Once the pillow reaches the end of the line, the direction changes, and people continue passing the pillow in the opposite direction.
    For example, once the pillow reaches the nth person they pass it to the n - 1th person, then to the n - 2th person and so on.
    Given the two positive integers n and time, return the index of the person holding the pillow after time seconds.

---
+++ google/2582_pass_the_pillow.py
@@ -0,0 +1,9 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        full_pass = time//(n-1) + 1
+        reminder = time%(n-1)
+        direction = full_pass%2
+        if direction == 1:
+            return reminder + 1
+        else:
+            return n - reminder
