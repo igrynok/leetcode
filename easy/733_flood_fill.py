656e24e28764b15a1616d4c8ca60447bb84c9231
igrynok <ibragimova.aliya@gmail.com>
2025-02-10 10:42:45 +0100

    You are given an image represented by an m x n grid of integers image, where image[i][j] represents the pixel value of the image. You are also given three integers sr, sc, and color. Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].

---
+++ easy/733_flood_fill.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def get_neighbours(self, cell, image, old_color):
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        neighbours = []
+        for d in directions:
+            new_cell = (cell[0] + d[0], cell[1] + d[1])
+            if  0 <= new_cell[0] < len(image) and 0 <= new_cell[1] < len(image[0]):
+                if image[new_cell[0]][new_cell[1]] == old_color:
+                    neighbours.append(new_cell)
+
+        return neighbours
+
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+
+        queue = deque([(sr, sc)])
+        old_color = image[sr][sc]
+        visited = set([(sr, sc)])
+        image[sr][sc] = color
+        while queue:
+            node = queue.popleft()
+            for n in self.get_neighbours(node, image, old_color):
+                if n not in visited:
+                    image[n[0]][n[1]] = color
+                    queue.append(n)
+                    visited.add(n)
+
+        return image
