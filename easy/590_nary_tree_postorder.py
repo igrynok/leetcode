209053afb449cc23fb48b2a8224d805bb36f083e
igrynok <ibragimova.aliya@gmail.com>
2025-02-11 08:39:57 +0100

    Given the root of an n-ary tree, return the postorder traversal of its nodes' values.

---
+++ easy/590_nary_tree_postorder.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+from typing import List
+
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+
+        result = []
+
+        def dfs(node):
+            if not node:
+                return
+            for child in node.children:
+                dfs(child)
+
+            result.append(node.val)
+
+        dfs(root)
+
+        return result
