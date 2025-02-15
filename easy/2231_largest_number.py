fc247cf64376ec229e6801d424b8e58ab1c67989
igrynok <ibragimova.aliya@gmail.com>
2025-02-15 10:42:18 +0100

    O(nlog(n) solution

--- easy/2231_largest_number.py
+++ easy/2231_largest_number.py
@@ -1,9 +1,16 @@
+from collections import deque
+
 class Solution:
     def largestInteger(self, num: int) -> int:
-        num = list(map(int, str(num)))
-        for i in range(len(num)):
-            for j in range(i + 1, len(num)):
-                if num[i] % 2 == num[j] % 2 and num[j] > num[i]:
-                    num[i], num[j] = num[j], num[i]
+        num_str = str(num)
+        even_digits = deque(sorted([d for d in num_str if int(d) % 2 == 0], reverse=True))
+        odd_digits = deque(sorted([d for d in num_str if int(d) % 2 == 1], reverse=True))
 
-        return int(''.join(map(str, num)))
+        result = []
+        for digit in num_str:
+            if int(digit) % 2 == 0:
+                result.append(even_digits.popleft())
+            else:
+                result.append(odd_digits.popleft())
+
+        return int(''.join(result))
