edf38a85636ff502a04fa0b2a6903c69b943ae55
Aliya Prokofyeva <igrynok@users.noreply.github.com>
2026-01-05 16:33:20 +0100

    Change iteration over set rather than list.

--- blind_75/128_longest_conseq_subseq.py
+++ blind_75/128_longest_conseq_subseq.py
@@ -7,7 +7,7 @@ class Solution:
         nums_set = set(nums)
         longest = 0
 
-        for num in nums:
+        for num in nums_set:
 
             if num - 1 not in nums_set:
                 current = 1
