fd21ebd8c6fa49fe3218cfcf8f198a508d09e301
Aliya Prokofyeva <igrynok@users.noreply.github.com>
2026-01-25 16:12:33 +0100

    You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given an integer k.
    Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
    
    Return the minimum possible difference.

---
+++ easy/1984_min_diff.py
@@ -0,0 +1,22 @@
+import math
+from typing import List
+
+
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+
+        if not nums:
+            return None
+
+        if len(nums) == 1:
+            return 0
+
+        nums.sort()
+
+        min_diff = math.inf
+        for i in range(len(nums)):
+            if i + k - 1 < len(nums):
+                min_diff = min(min_diff, nums[i + k - 1] - nums[i])
+
+        return min_diff
+
